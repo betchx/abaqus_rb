a874b3962be29db9d3169b6e16d1d07668651964
tanabe <tanabe.atsushi@gmail.com>
2011-06-28 15:31:33 +0900

    bindメソッド群をクラスに注入する部分をメソッドに分離．
    modelなしでもbindが使える様になる．

---
+++ lib/abaqus/binder.rb
@@ -0,0 +1,37 @@
+
+unless defined?(ABAQUS_BINDER_RB)
+  ABAQUS_BINDER_RB = true
+
+  module Abaqus
+    module Binder
+      module_function
+      def inject_bind_methods(target)
+        target.module_eval do
+          begin
+            class_variable_get(:@@method)
+          rescue
+            class_variable_set(:@@method, self.name.split(/::/).pop.downcase + "s")
+          end
+          class_variable_set(:@@old_bind, [])# unless defined?(:@@old_bind)
+          class_variable_set(:@@all, {})# unless defined?(:@@all)
+          def self.bind(model)
+            class_variable_get(:@@old_bind) << class_variable_get(:@@all)
+            class_variable_set(:@@all,
+                               model.__send__(class_variable_get(:@@method)))
+            #p self.class_variables
+            reset if defined?(reset)
+          end
+          def self.release
+            class_variable_set(:@@all, class_variable_get(:@@old_bind).pop)
+          end
+          def self.with_bind(model)
+            self.bind(mode)
+            yield
+            self.release
+          end
+        end
+      end
+    end
+  end
+
+end
--- lib/abaqus/model.rb
+++ lib/abaqus/model.rb
@@ -33,31 +33,14 @@ require pos + '/bc'
 require pos + '/load'
 require pos + '/step'
 require pos + '/material'
+require pos + '/binder'
 
 module Abaqus
   class Model
     BindTargets = [Node, Element, Nset, Elset,
       Bc, Load, Step, Property, Material]
     BindTargets.each do |target|
-      target.instance_eval do
-        begin
-          class_variable_get(:@@method)
-        rescue
-        class_variable_set(:@@method, self.name.split(/::/).pop.downcase + "s")
-        end
-        class_variable_set(:@@old_bind, [])# unless defined?(:@@old_bind)
-        class_variable_set(:@@all, {})# unless defined?(:@@all)
-        def self.bind(model)
-          class_variable_get(:@@old_bind) << class_variable_get(:@@all)
-          class_variable_set(:@@all,
-                             model.__send__(class_variable_get(:@@method)))
-          #p self.class_variables
-          reset if defined?(reset)
-        end
-        def self.release
-          class_variable_set(:@@all, class_variable_get(:@@old_bind).pop)
-        end
-      end
+      Binder.inject_bind_methods(target)
     end
     def with_bind
       bind_all
