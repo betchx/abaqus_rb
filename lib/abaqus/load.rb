6bfea792d4a76acdbf622b33db429c0209e139e3
tanabe <tanabe.atsushi@gmail.com>
2011-06-28 15:36:07 +0900

    Loadに最低限（エラーにならないだけ）のparseを追加．

--- lib/abaqus/load.rb
+++ lib/abaqus/load.rb
@@ -1,7 +1,17 @@
 
+unless defined?(ABAQUS_LOAD_RB)
+  ABAQUS_LOAD_RB = true
 
-module Abaqus
-  class Load
+  dir = File::dirname(__FILE__)
+  require dir + "/inp"
+
+  module Abaqus
+    class Load
+      extend Inp
+      def self.parse(head,body)
+        parse_data(body)
+      end
+    end
   end
-end
 
+end
