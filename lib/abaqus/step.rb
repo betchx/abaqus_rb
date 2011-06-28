4fa2d10a73f0940e4528fa31cb014e997a3c02a3
tanabe <tanabe.atsushi@gmail.com>
2011-06-28 15:42:16 +0900

    Stepに最低限のparseを追加．

--- lib/abaqus/step.rb
+++ lib/abaqus/step.rb
@@ -1,7 +1,51 @@
 
 
+dir = File::dirname(__FILE__)
+require dir + '/inp'
+
+unless defined?(Abaqus::Model)
+  require dir + '/binder'
+  [Abaqus::BC, ABAQUS::Load].each do |target|
+    Abaqus::Binder.inject_bind_methods(target)
+  end
+end
+
 module Abaqus
   class Step
+    @@all = []
+    extend Inp
+    def self.parse(head,body)
+      key, opts = parse_command(head)
+      unless key == "*STEP"
+        raise ArgumentError,
+          "Step.parse needs *STEP keyword but #{key} was given"
+      end
+      name = opts["NAME"] || "Step-#{@@all.size + 1}"
+      step = self.new(name)
+      # step has no data line
+      line = parse_data(body)
+      while line
+        key, opts = parse_command(line)
+        case key
+        when "*END STEP"
+          break
+        when "*BOUNDARY"
+          BC.with_bind(step) do
+            line = BC.parse(line, body)
+          end
+        when "*CLOAD","*DLOAD"
+          Load.with_bind(step) do
+            line = Load.parse(line, body)
+          end
+        else
+          line = parse_data(body)
+        end
+      end
+      return line, step
+    end
+    def bcs
+      @bcs||={}
+    end
     def initialize(name)
       @name = name
       @@all << self
