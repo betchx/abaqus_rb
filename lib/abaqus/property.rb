734cd838143529495920c99977880b39288cbc5d
tanabe <tanabe.atsushi@gmail.com>
2011-06-28 11:02:15 +0900

    オブジェクト生成とparseをひとまず実装

--- lib/abaqus/property.rb
+++ lib/abaqus/property.rb
@@ -1,6 +1,33 @@
 
+dir = File::dirname(__FILE__)
+require dir + "/inp"
+
 module Abaqus
   class Property
     @@method = "properties"
+    def initialize(elset_name, key, material, *values)
+      @name = elset_name
+      @material = material
+      @values = values
+      @@all[@name] = self
+    end
+    attr_reader :name, :material
+    def [](i)
+      return @values[i]
+    end
+
+    extend Inp
+    def self.parse(head, body)
+      key, opts = parse_command(head)
+      elset_name = opts["ELSET"]
+      raise if elset_name.nil?
+      mat = opts["MATERIAL"] || ""
+      vals = []
+      res = parse_data(body) do |str|
+        vals << str.split(/\s*,\s*/)
+      end
+      prop = self.new(elset_name, key, mat, props)
+      return res, prop
+    end
   end
 end
