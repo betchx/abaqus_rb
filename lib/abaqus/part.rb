a96d87bd734617d712883b4885b73788282ae423
Atsushi TANABE <tanabe.atsushi@nikken.jp>
2013-02-05 12:00:00 +0900

    Partにparseを実装．
    Abaqus::parseの縮小版の様な形で実装されている．
    KnowKeywordsはAbaqus::parseと供用.
    
    Bindの対象にPropertyもはいりうることがわかったため，追加．

--- lib/abaqus/part.rb
+++ lib/abaqus/part.rb
@@ -1,28 +1,35 @@
 
 dir = "abaqus" #File::dirname(__FILE__)
-require dir + 'node'
-require dir + 'nset'
-require dir + 'element'
-require dir + 'elset'
+require dir + '/node'
+require dir + '/nset'
+require dir + '/element'
+require dir + '/elset'
 require dir + '/binder'
+require dir + '/inp'
 
 unless defined?(ABAQUS_PART_RB)
   ABAQUS_PART_RB = true
 
   module Abaqus
     class Part
-
-
+      extend Inp
+      @@all = UpcaseHash.new
       def initialize(name)
         @nodes = {}
         @elements = {}
         @nsets = UpcaseHash.new
         @elsets = UpcaseHash.new
-        @name = name
+        @properties = UpcaseHash.new
+        @name = name.upcase
+        @@all[name.upcase] = self
       end
-      attr_reader nodes, elements, nsets, elsets, name
+      def self.[](name) @@all[name.upcase] end
+      def self.clear() @@all.clear end
+      def self.size() @@all.size end
 
-      BindTargets = [Node, Element, Nset, Elset]
+      attr_reader :nodes, :elements, :nsets, :elsets, :name, :properties
+
+      BindTargets = [Node, Element, Nset, Elset, Property]
 
       def bind_all
         BindTargets.each do |klass|
@@ -38,6 +45,27 @@ unless defined?(ABAQUS_PART_RB)
         bind_all
         yield
         release_all
+      end
+
+      def self.parse(line, body)
+        line
+        keyword, opt = parse_command(line) 
+        raise unless  keyword == "*PART"
+        name = opt['NAME']
+        part = Part.new(name)
+        line = parse_data(body) # move to contents
+        part.with_bind do
+          until keyword == "*END PART"
+            keyword, opt = parse_command(line)
+            klass = KnownKeywords[keyword]
+            if klass
+              line, *arg = klass.parse(line, body)
+            else
+              line = parse_data(body){} #skip unknown keyword
+            end
+          end
+        end
+        return line
       end
     end
   end
